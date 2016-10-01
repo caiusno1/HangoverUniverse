22fca89ca9c0f2b15a46a59a87e4945bd60a9a91
Timo Deutschmann <timodeutschmann@googlemail.com>
2016-10-01 19:26:15 +0200

    Nochmal :-)

--- js/preload.js
+++ js/preload.js
@@ -66,6 +66,7 @@ preload.prototype = {
         this.game.load.image("oxygenbar", "assets/hud/oxygenbar.png");
         this.game.load.image("heartImg", "assets/hud/heartImg.png");
         this.game.load.image("hungerImg", "assets/hud/hungerImg.png");
+        this.game.load.image("oxygenImg", "assets/hud/oxygenImg.png");
 
         //Audio
         //GameTitle / GameOver Background Sound
